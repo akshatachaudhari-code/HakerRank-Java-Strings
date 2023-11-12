5560328b47fb2e7b5d78715d159bb7daaff5f9e5
akshatachaudhari-code <116259547+akshatachaudhari-code@users.noreply.github.com>
2023-11-13 00:21:39 +0530

    Java Substring
    Output contains two space-separated integers denoting the respective values of Start and End .

---
+++ HakerRank-Java Substring.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        String c;
+        c=sc.next();
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(c.substring(a,b));
+    }
+}
